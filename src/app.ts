cf550c2c07cc08da550c38c4afa8514e7785f16d
Eduardo <eduardo.sbassani@gmail.com>
2024-08-27 19:32:23 +0300

    Adding reading from .env

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 const port = 8080;
+
+const {GEMINI_API_KEY} = process.env;
+
+if (!GEMINI_API_KEY) {
+  throw new Error('GEMINI_API_KEY não foi definido');
+}
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`http://localhost:${port}`);
 });
